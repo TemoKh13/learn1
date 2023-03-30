7ae9ceed4145ebda832035c7f61e0ef10c75feca
TemoKh13 <temukakhaburdzania@gmail.com>
2023-03-31 01:30:06 +0400

    LEARN COMMIG 3/31/2023

--- learn1/Program.cs
+++ learn1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Common;
+using System.IO;
 
 namespace learn1
 {
@@ -18,22 +20,95 @@ namespace learn1
             //Console.ReadLine();
 
 
-            Jariskaci[] otxiJariskaci = new Jariskaci[] 
-            {
-                new Jariskaci(),
-                new Jariskaci(),
-                new Jariskaci(),
-                new Jariskaci()
-            };
+            /*            Jariskaci[] otxiJariskaci = new Jariskaci[] 
+                        {
+                            new Jariskaci(),
+                            new Jariskaci(),
+                            new Jariskaci(),
+                            new Jariskaci()
+                        };
 
-            for (int index = 0; index <= 4; index++)
+                        for (int index = 0; index <= 4; index++)
+                        {
+                            otxiJariskaci[index].QvisSrola();
+                        }*/
+
+            /*            Console.WriteLine(Jariskaci.qvisSrolisRaodenoba);*/
+
+            /*            var str = "Me var {0} mqvia {1} gvari";
+                        string.Format(str, "Aleko", "Gvinefadze kai bichia");
+
+                        Console.WriteLine(str);*/
+
+
+/*            string stringValue = "Baro bichebi";
+            int indexOf = stringValue.IndexOf("bichebi");
+            string saxeli = "Bacho";
+            string gvari = " Gigiadze";
+            string concat = string.Concat(saxeli, gvari);
+            int index = 5;
+            string str1 = "temuka";
+            string str2 = "temuka";
+
+            bool udris1 = str1 == str2;
+            bool udirs2 = str1.Equals(str2);
+
+            string enumator = @"red,green,black,grey";
+            string[] splitedEnumator = enumator.Split(',');
+            foreach (var item in splitedEnumator)
             {
-                otxiJariskaci[index].QvisSrola();
+                Console.Write($"{item},");    
             }
 
-            Console.WriteLine(Jariskaci.qvisSrolisRaodenoba);
+            if (5 == 7)
+                Console.WriteLine("ar sheva");
+            else 
+                Console.WriteLine("sheva");
+
+            int pasuxiShedarebis = 5 == 7 
+                ? 1 
+                : 0;
+
+            double myDouble = 9.78;
+            int myInt = (int)myDouble;
+            double roundedDouble = Math.Round(myDouble);
+
+            string doubleStr = "9.78";
+            string strD = "9,4b";
+            var sum = doubleStr + 3;*/
+/*            try
+            {
+                int x = 0;
+                var cvaladi = 2 / x;
+            }
+            catch (Exception exception)
+            {
+                DivideByZeroException divideByZeroException = (DivideByZeroException)exception;
+                DivideByZeroException divideByZeroException2 = exception as DivideByZeroException;
+                Console.WriteLine("zogadi shecdoma");
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine("Uechveli aq var me dzma ver gameqcevi");
+            }*/
+
+
+
+            //bool isParsed = double.TryParse(strD, out var dob);
+/*            decimal cvladi = 7.0m / 9.0m;
+            decimal damrgvaleba = Math.Round(cvladi, 2);*/
+
+
+
+            string nikusha = "NIKU";
+            nikusha.GetHashCode();
+
+
             Console.ReadLine();
         }
 
     }
+
+
 }
